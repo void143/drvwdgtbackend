c205b12d87d6423c8c61dbfb14c55475e3d92029
Ivan Miskevich <ivan.miskevich@gmail.com>
2023-06-30 11:42:38 +0300

    Added stream fs http

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ module.exports = {
       async_hooks: false,
       zlib: require.resolve('browserify-zlib'),
       querystring: require.resolve('querystring-es3'),
-      crypto: require.resolve('crypto-browserify')
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      http: require.resolve('stream-http')
     }
   }
 };
