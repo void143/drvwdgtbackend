55dca2e4636511f7950d189a7f35f4d748992b14
Ivan Miskevich <ivan.miskevich@gmail.com>
2023-06-30 11:31:57 +0300

    Added a few resolve to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
   entry: './index.js'
+  resolve: {
+    fallback: {
+      async_hooks: false,
+      zlib: require.resolve('browserify-zlib'),
+      querystring: require.resolve('querystring-es3'),
+      crypto: require.resolve('crypto-browserify')
+    }
+  }
 };
